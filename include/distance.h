f2cd574385e93fc65f076c08f006395c97d9c649
29a <2651419874@qq.com>
2025-08-31 19:45:02 +0800

    add AVX-512

--- include/distance.h
+++ include/distance.h
@@ -10,52 +10,86 @@ namespace stkq
     class E_Distance
     {
     public:
+        // template <typename T>
+        // inline float sqr_dist(const T *d, const T *q, unsigned L) const
+        // {
+        //     float PORTABLE_ALIGN32 TmpRes[8] = {0};
+        //     uint32_t num_blk16 = L >> 4;
+        //     uint32_t l = L & 0b1111;
+
+        //     __m256 diff, v1, v2;
+        //     __m256 sum = _mm256_set1_ps(0);
+        //     for (uint32_t i = 0; i < num_blk16; i++)
+        //     {
+        //         v1 = _mm256_loadu_ps(d);
+        //         v2 = _mm256_loadu_ps(q);
+        //         d += 8;
+        //         q += 8;
+        //         diff = _mm256_sub_ps(v1, v2);
+        //         sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+
+        //         v1 = _mm256_loadu_ps(d);
+        //         v2 = _mm256_loadu_ps(q);
+        //         d += 8;
+        //         q += 8;
+        //         diff = _mm256_sub_ps(v1, v2);
+        //         sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+        //     }
+        //     for (uint32_t i = 0; i < l / 8; i++)
+        //     {
+        //         v1 = _mm256_loadu_ps(d);
+        //         v2 = _mm256_loadu_ps(q);
+        //         d += 8;
+        //         q += 8;
+        //         diff = _mm256_sub_ps(v1, v2);
+        //         sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+        //     }
+        //     _mm256_store_ps(TmpRes, sum);
+
+        //     float ret = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] +
+        //                 TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
+
+        //     for (uint32_t i = 0; i < l % 8; i++)
+        //     {
+        //         float tmp = (*q) - (*d);
+        //         ret += tmp * tmp;
+        //         d++;
+        //         q++;
+        //     }
+        //     return ret;
+        // }
         template <typename T>
-        inline float sqr_dist(const T *d, const T *q, unsigned L) const
-        {
-            float PORTABLE_ALIGN32 TmpRes[8] = {0};
-            uint32_t num_blk16 = L >> 4;
-            uint32_t l = L & 0b1111;
+        inline float sqr_dist(const T* d, const T* q, unsigned L) const {
+            alignas(64) float TmpRes[16] = {0}; // AVX-512 一次 512 bit = 16 x float
+            uint32_t num_blk16 = L >> 4;        // 16 个 float 一块
+            uint32_t l = L & 0b1111;            // 剩余元素数
 
-            __m256 diff, v1, v2;
-            __m256 sum = _mm256_set1_ps(0);
-            for (uint32_t i = 0; i < num_blk16; i++)
-            {
-                v1 = _mm256_loadu_ps(d);
-                v2 = _mm256_loadu_ps(q);
-                d += 8;
-                q += 8;
-                diff = _mm256_sub_ps(v1, v2);
-                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+            __m512 diff, v1, v2;
+            __m512 sum = _mm512_set1_ps(0.0f);  // 初始化 sum = 0
 
-                v1 = _mm256_loadu_ps(d);
-                v2 = _mm256_loadu_ps(q);
-                d += 8;
-                q += 8;
-                diff = _mm256_sub_ps(v1, v2);
-                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+            // 每次处理 16 个 float
+            for (uint32_t i = 0; i < num_blk16; i++) {
+                v1 = _mm512_loadu_ps(d);
+                v2 = _mm512_loadu_ps(q);
+                d += 16;
+                q += 16;
+                diff = _mm512_sub_ps(v1, v2);
+                sum = _mm512_fmadd_ps(diff, diff, sum); // sum += diff * diff
             }
-            for (uint32_t i = 0; i < l / 8; i++)
-            {
-                v1 = _mm256_loadu_ps(d);
-                v2 = _mm256_loadu_ps(q);
-                d += 8;
-                q += 8;
-                diff = _mm256_sub_ps(v1, v2);
-                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
-            }
-            _mm256_store_ps(TmpRes, sum);
 
-            float ret = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] +
-                        TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
+            // 把 SIMD 累积结果存回数组
+            _mm512_store_ps(TmpRes, sum);
 
-            for (uint32_t i = 0; i < l % 8; i++)
-            {
-                float tmp = (*q) - (*d);
+            // 汇总 SIMD 累加的 16 个结果
+            float ret = 0.0f;
+            for (int i = 0; i < 16; i++) ret += TmpRes[i];
+
+            // 处理剩余不足 16 个的元素
+            for (uint32_t i = 0; i < l; i++) {
+                float tmp = q[i] - d[i];
                 ret += tmp * tmp;
-                d++;
-                q++;
             }
+
             return ret;
         }
 
